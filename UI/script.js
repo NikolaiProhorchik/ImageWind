0a104a9b6a5aa709e9b7772141bee111b377c4d8
Prohorchik Nikolai <prohorchik-37@tut.by>
2019-03-11 18:05:46 +0300

    add script.js

---
+++ UI/script.js
@@ -0,0 +1,582 @@
+;(function() {
+	var PhotoPosts = [
+		{
+            id: '1',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+		},
+		{
+            id: '2',
+            description: 'Красная панда, Китай',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '3',
+            description: 'Письменные упоминания об этом звере в Китае восходят к XIII столетию',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '4',
+            description: 'Красная панда',
+            createdAt: new Date('2016-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '5',
+            description: 'Красная панда',
+            createdAt: new Date('2017-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '6',
+            description: 'Красная панда',
+            createdAt: new Date('2014-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '7',
+            description: 'Красная панда',
+            createdAt: new Date('2008-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '8',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '9',
+            description: 'Красная панда',
+            createdAt: new Date('2003-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '10',
+            description: 'Красная панда',
+            createdAt: new Date('2021-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '11',
+            description: 'Красная панда',
+            createdAt: new Date('2039-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '12',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '13',
+            description: 'Красная панда',
+            createdAt: new Date('2001-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '14',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Красивый зверёк'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '15',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '16',
+            description: 'Красная панда',
+            createdAt: new Date('2010-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '17',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Симпотяга'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '18',
+            description: 'Красная панда',
+            createdAt: new Date('2016-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '19',
+            description: 'Красная панда',
+            createdAt: new Date('2019-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+		{
+            id: '20',
+            description: 'Красная панда',
+            createdAt: new Date('2020-03-03T13:09:00'),
+            author: 'Mike Phillips',
+            photoLink: 'Panda.jpg',
+            hashTags: ['КраснаяПанда','Зверь из Китая'],
+            likes:['Ann','Егор']
+        },
+    ];
+	
+	function getPhotoPosts(skip = 0, top = 10, filterConfig)
+    {
+		var PhotoPostsFiltered = [];
+		
+		if ((skip >= 0 && skip < PhotoPosts.length) && (top > 0 && top <= PhotoPosts.length))
+		{
+			if (filterConfig != undefined)
+            {
+				if ('author' in filterConfig && filterConfig.author != undefined)
+                {
+					if ('createdAt' in filterConfig && filterConfig.createdAt != undefined)
+					{
+						if ('hashTags' in filterConfig && filterConfig.hashTags != undefined)
+						{
+							for (var i = 0; i < PhotoPosts.length; i++) 
+							{
+								if (PhotoPosts[i].author == filterConfig.author && PhotoPosts[i].createdAt == filterConfig.createdAt)
+								{
+									pass:
+									for (var k = 0; k < filterConfig.hashtags.length; k++) 
+									{
+										for (var j = 0; j < PhotoPosts[i].hashtags.length; j++) 
+										{
+											if (filterConfig.hashtags[k] == PhotoPosts[i].hashtags[j])
+											{
+												PhotoPostsFiltered.push(PhotoPosts[i]);
+												break pass;
+											}
+										}
+									}
+								}
+							}
+						}
+						else
+						{
+							for (var i = 0; i < PhotoPosts.length; i++) 
+							{
+								if (PhotoPosts[i].author == filterConfig.author && PhotoPosts[i].createdAt == filterConfig.createdAt)
+								{
+									PhotoPostsFiltered.push(PhotoPosts[i]);
+								}
+							}
+						}
+					}
+					else
+					{
+						if ('hashTags' in filterConfig && filterConfig.hashTags != undefined)
+						{
+							for (var i = 0; i < PhotoPosts.length; i++) 
+							{
+								if (PhotoPosts[i].author == filterConfig.author)
+								{
+									pass:
+									for (var k = 0; k < filterConfig.hashtags.length; k++) 
+									{
+										for (var j = 0; j < PhotoPosts[i].hashtags.length; j++) 
+										{
+											if (filterConfig.hashtags[k] == PhotoPosts[i].hashtags[j])
+											{
+												PhotoPostsFiltered.push(PhotoPosts[i]);
+												break pass;
+											}
+										}
+									}
+								}
+							}
+						}
+						else
+						{
+							for (var i = 0; i < PhotoPosts.length; i++) 
+							{
+								if (PhotoPosts[i].author == filterConfig.author)
+								{
+									PhotoPostsFiltered.push(PhotoPosts[i]);
+								}
+							}
+						}
+					}
+                }
+				else
+				{
+					if ('createdAt' in filterConfig && filterConfig.createdAt != undefined)
+					{
+						if ('hashTags' in filterConfig && filterConfig.hashTags != undefined)
+						{
+							for (var i = 0; i < PhotoPosts.length; i++)
+							{
+								if (PhotoPosts[i].createdAt == filterConfig.createdAt)
+								{
+									pass:
+									for (var k = 0; k < filterConfig.hashtags.length; k++) 
+									{
+										for (var j = 0; j < PhotoPosts[i].hashtags.length; j++) 
+										{
+											if (filterConfig.hashtags[k] == PhotoPosts[i].hashtags[j])
+											{
+												PhotoPostsFiltered.push(PhotoPosts[i]);
+												break pass;
+											}
+										}
+									}
+								}
+							}
+						}
+						else
+						{
+							for (var i = 0; i < PhotoPosts.length; i++) 
+							{
+								if (PhotoPosts[i].createdAt == filterConfig.createdAt)
+								{
+									PhotoPostsFiltered.push(PhotoPosts[i]);
+								}
+							}
+						}
+					}
+					else
+					{
+						if ('hashTags' in filterConfig && filterConfig.hashTags != undefined)
+						{
+							for (var i = 0; i < PhotoPosts.length; i++)
+							{
+								pass:
+								for (var k = 0; k < filterConfig.hashtags.length; k++) 
+								{
+									for (var j = 0; j < PhotoPosts[i].hashtags.length; j++) 
+									{
+										if (filterConfig.hashtags[k] == PhotoPosts[i].hashtags[j])
+										{
+											PhotoPostsFiltered.push(PhotoPosts[i]);
+											break pass;
+										}
+									}
+								}
+							}
+						}
+						else
+						{
+							PhotoPostsFiltered = PhotoPosts;
+						}
+					}
+				}
+			}
+			else
+			{
+				PhotoPostsFiltered = PhotoPosts;
+			}
+			
+			PhotoPostsFiltered.sort(function (a, b)
+            {
+                return new Date(b.createdAt) - new Date(a.createdAt);
+            });
+          
+            PhotoPostsFiltered = PhotoPostsFiltered.slice(skip, skip + top);
+			return PhotoPostsFiltered;
+		}
+		else
+		{
+			return PhotoPostsFiltered;
+		}
+	}
+	
+	function getPhotoPost(id)
+    {
+        var PhotoPost;
+        for (var i = 0; i < PhotoPosts.length; i++)
+		{
+            if (PhotoPosts[i].id == id)
+            {
+                PhotoPost = PhotoPosts[i];
+                return PhotoPost;
+            }
+        }
+		
+        PhotoPost =
+            {
+                id: 'notfound',
+				description: 'notfound',
+                createdAt: new Date('0001-01-01T00:00:00'),
+                author: 'notfound',
+                photoLink: 'notfound',
+                hashtags: ['notfound'],
+                likes:['notfound']
+             };
+        return PhotoPost;
+    }
+
+	function validatePhotoPost(PhotoPost)
+    {
+        if(PhotoPost === undefined)
+        {
+            return false;
+        }
+        
+		var TYPESTRING = '[object String]';
+        var TYPEDATE = '[object Date]';
+        var TYPEOBJECT = '[object Array]';
+        var toString = {}.toString;
+        
+		return 	(TYPESTRING == toString.call(PhotoPost.id))&&
+		(TYPESTRING == toString.call(PhotoPost. description))&&
+        (TYPESTRING == toString.call(PhotoPost.author))&&
+		(TYPEDATE == toString.call(PhotoPost.createdAt))&&
+		(PhotoPost.createdAt != 'Invalid Date')&&
+		(TYPESTRING == toString.call(PhotoPost.photoLink))&&
+		(TYPEOBJECT == toString.call(PhotoPost.hashtags))&&
+		(TYPEOBJECT == toString.call(PhotoPost.likes))
+    }
+
+	function addPhotoPost(PhotoPost)
+    {
+        if(validatePhotoPost(PhotoPost))
+        {
+            PhotoPosts.push(PhotoPost);
+            return true;
+        }
+        return false;
+    }
+	
+	function editPhotoPost(id, PhotoPost)
+    {
+		let index = getIndexId(id);
+		
+		if(index == -1)
+		{
+           return false;
+		}
+
+		if(PhotoPost.description != undefined)
+		{
+			var TYPESTRING = '[object String]';
+			var toString = {}.toString;
+			
+			if (TYPESTRING == toString.call(PhotoPost. description))
+			{
+				PhotoPosts[index].description = PhotoPost.description;
+			}
+			else
+			{
+				return false;
+			}
+		}
+      
+        if(PhotoPost.photoLink != undefined)
+        {
+			var TYPESTRING = '[object String]';
+			var toString = {}.toString;
+			
+			if(TYPESTRING == toString.call(PhotoPost.photoLink))
+			{
+				PhotoPosts[index].photoLink = PhotoPost.photoLink;
+			}
+			else
+			{
+				return false;
+			}
+        }
+		
+		if(PhotoPost.hashTags != undefined)
+        {
+			var TYPESTRING = '[object String]';
+			var toString = {}.toString;
+			
+			if(TYPEOBJECT == toString.call(PhotoPost.hashtags))
+			{
+				PhotoPosts[index].hashTags = PhotoPost.hashTags;
+			}
+			else
+			{
+				return false;
+			}
+        }
+		
+		return true;
+    }
+
+	function removePhotoPost(id)
+    {
+		let tempId = getIndexId(id);
+        
+		if(tempId != -1)
+		{
+            PhotoPosts.splice(tempId, 1);
+            return true;
+        }
+		return false;
+    }
+
+	function getIndexId(id)
+    {
+        for (var i = 0; i < PhotoPosts.length; i++)
+        {
+            if (PhotoPosts[i].id == id)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+	
+	PhotoPosts.getPhotoPosts = getPhotoPosts;
+    PhotoPosts.getPhotoPost = getPhotoPost;
+    PhotoPosts.validatePhotoPost = validatePhotoPost;
+    PhotoPosts.addPhotoPost = addPhotoPost;
+    PhotoPosts.editPhotoPost = editPhotoPost;
+    PhotoPosts.removePhotoPost = removePhotoPost;
+    PhotoPosts.getIndexId = getIndexId;
+	
+	var PhotoPostsArr;
+    var PhotoPost;
+    
+	//////////////////////getPhotoPosts:
+    console.log('Проверка getPhotoPosts с двумя валидными параметрами:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(0,5);
+    console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts с тремя валидными параметрами:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(0,3,{author:'Mike Phillips', hashtags:['КраснаяПанда','Зверь из Китая']});
+	console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts с тремя валидными параметрами:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(0,5,{hashtags:['КраснаяПанда','Зверь из Китая']});
+    console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts с первым инвалидным параметром:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(-1,5,{author:'Mike Phillips'});
+    console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts со вторым инвалидным параметром:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(1,100,{author:'Mike Phillips', hashtags:['КраснаяПанда','Зверь из Китая']});
+    console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts с двумя инвалидными параметром:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(-2,100,{author:'Mike Phillips'});
+    console.log(PhotoPostsArr);
+    console.log('Проверка getPhotoPosts когда по третьему параметру ничего не подходит:');
+    PhotoPostsArr = PhotoPosts.getPhotoPosts(5,10,{author:'Mikesdhgf gdfggPhillips', hashtags:['КраснаяПанда','Зверь из Китая']});
+    console.log(PhotoPostsArr);
+	
+	//////////////////////////getPhotoPost
+    console.log('Проверка getPhotoPost с существующим индексом:');
+    PhotoPost = PhotoPosts.getPhotoPost(3);
+    console.log(PhotoPost);
+    console.log('Проверка getPhotoPost с несуществующим индексом:');
+    PhotoPost = PhotoPosts.getPhotoPost(25);
+    console.log(PhotoPost);
+    
+	////////////////////////////validatePhotoPost/addPhotoPost
+    console.log('Проверка методов validatePhotoPost и addPhotoPost с валидным объектом:');
+    var PhotoPostIfValidate =
+    {
+        id: '111',
+        description: 'Что-нибудь',
+        createdAt: new Date('2357-02-23T23:00:00'),
+        author: 'EgorSem',
+        photoLink: 'Panda.jpg',
+        hashtags: ['Красивый зверёк','панда'],
+        likes:['Ann','Егор']
+    }
+	console.log('Валидность элемента:');
+    console.log(PhotoPosts.validatePhotoPost(PhotoPostIfValidate));
+    console.log('Добавление валидного элемента:');
+    console.log(PhotoPosts.addPhotoPost(PhotoPostIfValidate));
+    console.log(PhotoPosts);
+    console.log('Проверка методов validatePhotoPost и addPhotoPost с не валидным объектом:');
+    PhotoPostIfValidate =
+        {
+            id: '-399',
+            description: 'И ещё что-нибудь(ЭЛЕМЕНТ НЕ ДОБАВЛЯЕТСЯ)',
+            createdAt: new Date('2357-02-23m23:00:00'),
+            author: 'EgorSem',
+            photoLink: 'Panda.jpg',
+            hashtags: ['Красивый зверёк','панда'],
+            likes:['Ann','Егор']
+        }
+	console.log('Валидность элемента:');
+    console.log(PhotoPosts.validatePhotoPost(PhotoPostIfValidate));
+    console.log('Добавление инвалидного элемента:');
+    console.log(PhotoPosts.addPhotoPost(PhotoPostIfValidate));
+    console.log(PhotoPosts);
+    
+	///////////////////////editPhotoPost
+    console.log('Проверка метода editPhotoPost:');
+    var PhotoPostEdit =
+        {
+			id: '300',
+            description: 'И снова что-нибудь',
+            createdAt: new Date('2369-03-23T23:00:00'),
+            author: 'EgorSem',
+            photoLink: 'Panda.jpg',
+            hashtags: ['Красивый зверёк','панда'],
+            likes:['Ann','Егор']
+        };
+    console.log(PhotoPostEdit);
+	PhotoPosts.addPhotoPost(PhotoPostEdit);
+    console.log('После редактирования:');
+    console.log(PhotoPosts.editPhotoPost(300,{description: 'Новое описание', photoLink: 'Новая фотка.jpg'}));
+    console.log(PhotoPosts);
+    
+	/////////////////removePhotoPost
+    console.log('Проверка метода removePhotoPost:');
+    console.log(PhotoPosts.removePhotoPost(2));
+    console.log(PhotoPosts);
+}());
